b11400a06a3be3bd25e123328f483361e14e1f1c
Leonid <leonid.vilcu@vlc2.com>
2023-07-07 12:17:24 +0200

    finito1

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,10 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
+        String string = "Write code that replaces the word cat with the word dog wherever it shows up in a sentence";
+        System.out.println(string);
 
-        ArrayList<String> animali = new ArrayList<>();
-
-        animali.add("leone");
-        animali.add("gazzella");
-        animali.add("gatto");
-        animali.add("elefante");
-
-
-        for (int i = 0; i < animali.size(); i++){
-            System.out.println(animali.get(i));
-
-        }
-
-        animali.set(2,"cane");
-
-        for (int i = 0; i < animali.size(); i++){
-            System.out.println(animali.get(i));
-        }
+        String risultato = string.replaceAll("cat", "dog");
+        System.out.println(risultato);
     }
 }
